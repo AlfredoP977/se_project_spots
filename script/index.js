71d35a115298ff379f61bfb7005e559f06223775
AlfredoP977 <A_parodi97@hotmail.com>
2024-10-15 21:20:38 -0400

    java close and open

--- script/index.js
+++ script/index.js
@@ -25,3 +25,19 @@ const initialCards = [
   },
 ];
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-Profile-Modal");
+const modalCloseButton = document.querySelector(".modal__close-btn");
+
+function openModal() {
+  editProfileModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editProfileModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+modalCloseButton.addEventListener("click", closeModal);
